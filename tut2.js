6f9738e63cfcc71b71f878a5aecbc160067c09f1
rajeshkumar34 <90446351+rajeshkumar34@users.noreply.github.com>
2021-12-13 17:22:17 +0530

    First Commit

---
+++ tut2.js
@@ -0,0 +1,52 @@
+console.log("this is a tut2.js");
+
+// primitive data types
+// string
+
+let name ="rajesh";
+console.log("My  string is " + name); 
+console.log("Date type is " + (typeof name));  
+
+// number
+let marks =53;
+console.log("Date tyoe is "+ (typeof marks));
+
+// Boolean
+let isDriver =true;
+console.log("Date tyoe is "+ (typeof isDriver));
+
+// Nul
+let nullvar =null;
+console.log("data type is "+ (typeof nullvar));
+
+// Undefined
+let undef = undefined;
+console.log("data type is "+ (typeof undefined));
+
+// Reference Data types
+
+// Arreys
+myarr =[2,1,3,4]; 
+console.log("data type is "+ (typeof myarr));
+
+// Object Listerals
+let stMarks ={
+  rajesh:80,
+  shubham: 32,
+  Rohan: 36,
+}
+console.log(stMarks);
+
+// Function
+function findName(){
+
+}
+console.log(typeof findName);
+
+// Dates
+
+let date = new Date();
+console.log(typeof date);
+
+
+
---
+++ tut3.js
@@ -0,0 +1,31 @@
+console.log('this is tut3');
+// variablein in js
+// var, let ,const
+var name ='Rajesh';
+var channel2;
+channel2 ='codewithladi';
+var marks=3245;
+console.log(name,channel2,marks)
+// Rules for creating javascript variable
+
+/*
+1.connot start with number
+2.can start with letter, number, _ or $
+3.are case sensitive
+*/
+var city='patiala'
+console.log(city)
+
+const ownersname ="Rjaesh kumar"
+console.log(ownersname)
+{
+  let city="Rampur";
+  console.log(city)
+}
+console.log(city)
+
+const arrl =[13,12,16,54,34];
+arrl.push(45);
+console.log(arrl)
+
+ 
